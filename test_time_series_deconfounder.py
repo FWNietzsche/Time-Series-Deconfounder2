d93b5c1cffc6b3b76e65e2f88055da7d19152018
FWNietzsche <utopie9090@snu.ac.kr>
2021-11-03 18:18:34 +0900

    test

---
+++ test_time_series_deconfounder.py
@@ -0,0 +1,7 @@
+
+python main_time_series_deconfounder.py
+
+python main_time_series_deconfounder.py --gamma=0.6 --exp_name='test_tsd_gamma_0.6' \
+--num_simulated_hidden_confounders=1 --num_simulated_hidden_confounders=1  --results_dir='results'
+
+
