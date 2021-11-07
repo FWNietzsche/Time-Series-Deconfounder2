0358f0d8e03faa3d06f7306535b15fa6a8775bb5
FWNietzsche <utopie9090@snu.ac.kr>
2021-11-07 22:47:48 +0900

    test test2 test3

--- test_time_series_deconfounder.py
+++ test_time_series_deconfounder.py
@@ -1,5 +1,3 @@
 
-python main_time_series_deconfounder.py --exp_name='trying'
+python main_time_series_deconfounder.py --gamma=0.6 --exp_name='trying' \
 --num_simulated_hidden_confounders=1 --num_simulated_hidden_confounders=1  --results_dir='results2'
-
-
