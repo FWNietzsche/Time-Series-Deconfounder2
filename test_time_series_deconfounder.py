8a0b777cc896c5fd7d6570b161ce257da83df5b4
FWNietzsche <utopie9090@snu.ac.kr>
2021-11-08 04:13:50 +0900

    test test2 test3

--- test_time_series_deconfounder.py
+++ test_time_series_deconfounder.py
@@ -1,3 +1,9 @@
 
+# from simulated_autoregressive import AutoregressiveSimulation
+# autoregressive = AutoregressiveSimulation(args.gamma, args.num_simulated_hidden_confounders)
+# dataset = autoregressive.generate_dataset(5000, 31)
+#
+# print(dataset)
+
 python main_time_series_deconfounder.py --gamma=0.6 --exp_name='trying' \
 --num_simulated_hidden_confounders=1 --num_simulated_hidden_confounders=1  --results_dir='results2'
