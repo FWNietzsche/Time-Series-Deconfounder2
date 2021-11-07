4862f4502870cb6c72ea47e1cb851a7ff20f7737
FWNietzsche <utopie9090@snu.ac.kr>
2021-11-07 22:38:30 +0900

    test test2 test3

--- test_time_series_deconfounder.py
+++ test_time_series_deconfounder.py
@@ -1,7 +1,5 @@
 
-python main_time_series_deconfounder.py
-
-python main_time_series_deconfounder.py --exp_name='test' \
---num_simulated_hidden_confounders=1 --num_simulated_hidden_confounders=1  --results_dir='results' --eval
+python main_time_series_deconfounder.py --exp_name='trying'
+--num_simulated_hidden_confounders=1 --num_simulated_hidden_confounders=1  --results_dir='results2'
 
 
